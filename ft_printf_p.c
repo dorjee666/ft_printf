79a9d8e9497922461b2331a5279b7005651502fe
Dorjee Phuntsok <dphuntso@e1z3r3p10.42.us.org>
2018-06-03 15:16:46 -0700

    43

--- ft_printf_p.c
+++ ft_printf_p.c
@@ -6,7 +6,7 @@
 /*   By: dphuntso <dphuntso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 11:42:30 by dphuntso          #+#    #+#             */
-/*   Updated: 2018/06/03 11:59:09 by dphuntso         ###   ########.fr       */
+/*   Updated: 2018/06/03 15:15:56 by dphuntso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,6 @@ void	ft_printf_conversion_p(t_arg *arg)
 	num = (unsigned long long)va_arg(arg->ap, unsigned long long int);
 	str = ft_itoa_base(num, 16);
 	ft_printf_p_help(arg, str);
-	// free(str);
-	// str = NULL;
+	free(str);
+	str = NULL;
 }
