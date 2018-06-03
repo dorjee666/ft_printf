3ec60b8860d6265c172d9496e0cd7ce73c1b2fdc
Dorjee Phuntsok <dphuntso@e1z3r3p10.42.us.org>
2018-06-03 14:35:32 -0700

    40

--- ft_printf_s.c
+++ ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: dphuntso <dphuntso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 17:44:48 by dphuntso          #+#    #+#             */
-/*   Updated: 2018/06/03 14:08:44 by dphuntso         ###   ########.fr       */
+/*   Updated: 2018/06/03 14:32:33 by dphuntso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@ void	ft_printf_s_minus(char *str, int len, t_arg *arg)
 {
 	if (arg->precision <= len && arg->precision >= 0)
 	{
-		arg->ret += ft_printf_putstr(arg->precision, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putstr(arg->precision, str);
 		ft_printf_putchar(arg->width - arg->precision, " ", arg);
 	}
 	else
 	{
-		arg->ret += ft_printf_putstr(len, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putstr(len, str);
 		ft_printf_putchar(arg->width - len, " ", arg);
 	}
 }
@@ -31,12 +37,18 @@ void	ft_printf_s_nominus(char *str, int len, t_arg *arg)
 	if (arg->precision <= len && arg->precision >= 0)
 	{
 		ft_printf_putchar(arg->width - arg->precision, " ", arg);
-		arg->ret += ft_printf_putstr(arg->precision, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putstr(arg->precision, str);
 	}
 	else
 	{
 		ft_printf_putchar(arg->width - len, " ", arg);
-		arg->ret += ft_printf_putstr(len, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putstr(len, str);
 	}
 }
 
@@ -44,12 +56,18 @@ void	ft_printf_ws_minus(wchar_t *str, int len, t_arg *arg)
 {
 	if (arg->precision <= len && arg->precision >= 0)
 	{
-		arg->ret += ft_printf_putwstr(arg->precision, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putwstr(arg->precision, str);
 		ft_printf_putchar(arg->width - arg->precision, " ", arg);
 	}
 	else
 	{
-		arg->ret += ft_printf_putwstr(len, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putwstr(len, str);
 		ft_printf_putchar(arg->width - len, " ", arg);
 	}
 }
@@ -59,12 +77,18 @@ void	ft_printf_ws_nominus(wchar_t *str, int len, t_arg *arg)
 	if (arg->precision <= len && arg->precision >= 0)
 	{
 		ft_printf_putchar(arg->width - arg->precision, " ", arg);
-		arg->ret += ft_printf_putwstr(arg->precision, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putwstr(arg->precision, str);
 	}
 	else
 	{
 		ft_printf_putchar(arg->width - len, " ", arg);
-		arg->ret += ft_printf_putwstr(len, str);
+		if (!str)
+			arg->ret += ft_printf_putstr(6, "(null)");
+		else
+			arg->ret += ft_printf_putwstr(len, str);
 	}
 }
 
@@ -76,8 +100,7 @@ void	ft_printf_conversion_s(t_arg *arg, char *format)
 
 	if (arg->length[0] == 'l' || *format == 'S')
 	{
-		if ((str1 = va_arg(arg->ap, wchar_t *)) == NULL)
-			str1 = "(null)";
+		str1 = va_arg(arg->ap, wchar_t *);
 		len = ft_printf_wstrlen(str1);
 		if (arg->flag[1] == '-')
 			ft_printf_ws_minus(str1, len, arg);
@@ -85,8 +108,7 @@ void	ft_printf_conversion_s(t_arg *arg, char *format)
 			ft_printf_ws_nominus(str1, len, arg);
 		return ;
 	}
-	if ((str = va_arg(arg->ap, char *)) == NULL)
-		str = "(null)";
+	str = va_arg(arg->ap, char *);
 	len = (int)ft_strlen(str);
 	if (arg->flag[1] == '-')
 		ft_printf_s_minus(str, len, arg);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dphuntso <dphuntso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 21:27:27 by dphuntso          #+#    #+#             */
-/*   Updated: 2018/06/01 23:30:18 by dphuntso         ###   ########.fr       */
+/*   Updated: 2018/06/03 14:12:24 by dphuntso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,7 @@
 
 int		main(void)
 {
-	ft_printf("- -  -- -- - - - - -- - - - - - - -\n");
-	ft_printf("  %%.2s will be -> %.2sover\n", "hello");
-	ft_printf("%%-6.6s will be -> %2.5sover\n", "hello");
-	ft_printf("- -- -- - - -- - - - -- - - - - - -\n");
-	printf("- -  -- -- - - - - -- - - - - - - -\n");
-	printf("  %%.2s will be -> %.2sover\n", "hello");
-	printf("%%-6.6s will be -> %2.5sover\n", "hello");
-	printf("- -- -- - - -- - - - -- - - - - - -\n");
+	ft_printf("%s\n", NULL);
 	while (1)
 		write(1, "", 1);
 	return (1);
