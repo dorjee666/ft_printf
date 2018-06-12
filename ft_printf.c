287b74d0a8433f18771664ede48b6aa773ae606d
Dorjee Phuntsok <dphuntso@e1z3r3p16.42.us.org>
2018-06-11 18:25:02 -0700

    dont break

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dphuntso <dphuntso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/07 12:58:01 by dphuntso          #+#    #+#             */
-/*   Updated: 2018/06/09 19:34:01 by dphuntso         ###   ########.fr       */
+/*   Updated: 2018/06/11 18:23:39 by dphuntso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,8 +73,7 @@ int		ft_printf(const char *format, ...)
 		return (-1);
 	va_end(ap2);
 	ret = arg->ret;
-	if (!arg)
-		free(arg);
+	free(arg);
 	arg = NULL;
 	return (ret);
 }
